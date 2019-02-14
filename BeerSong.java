a6b26cd30b31cfe9d5137c605e36e49790527d6d
Andrew Cicchelli <ajcicchelli@oakland.edu>
2019-02-13 23:23:37 -0500

    BeerSong.java functions added (empty)

---
+++ BeerSong.java
@@ -0,0 +1,14 @@
+package Assignment2;
+
+/**
+ * Created by ChiliChinchilla on 2/13/2019.
+ */
+public class BeerSong {
+    public static void main(String[] args){
+        playSong();
+    }
+
+    public static void playSong() {
+    	
+    }
+}
