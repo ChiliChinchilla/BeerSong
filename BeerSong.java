03e34c5c716faff2ffb661bac92e969f0dd6ca08
Andrew Cicchelli <ajcicchelli@oakland.edu>
2019-02-13 23:41:36 -0500

    Finished main() and playSong()

--- BeerSong.java
+++ BeerSong.java
@@ -9,6 +9,23 @@ public class BeerSong {
     }
 
     public static void playSong() {
+    	int bottles = 99;
+    	String plur = "bottles";
     	
+    	while (bottles > 0) {
+    		
+    		System.out.println(bottles + " " + plur + " of beer on the wall, " + bottles + " " + plur + " of beer");
+    		bottles--;
+    		
+    		if (bottles == 1)
+    			plur = "bottle";
+    		else
+    			plur = "bottles";
+    		
+    		if (bottles == 0)
+    			System.out.println("Take one down, pass it around, no more bottles of beer on the wall.");
+    		else
+    			System.out.println("Take one down, pass it around, " + bottles + " " + plur + " of beer on the wall.");
+    	}
     }
 }
